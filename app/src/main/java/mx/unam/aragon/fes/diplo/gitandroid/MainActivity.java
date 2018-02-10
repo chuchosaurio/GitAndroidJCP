517c69323ad8ed813e7ccbfd72de10bc5af7d13f
chuchosaurio <j.chavez.perez.89@gmail.com>
2018-02-10 13:29:35 -0600

    Modificacion de boton y textView

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -2,12 +2,28 @@ package mx.unam.aragon.fes.diplo.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button miBoton;
+    private TextView miTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        miBoton = findViewById(R.id.button);
+        miTexto = findViewById(R.id.textView);
+
+        miBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                miTexto.setText("Hola GitHub");
+            }
+        });
     }
+
 }
