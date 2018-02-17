e41de9f8daa899fe9adb80bf0fe635989f84c80b
chuchosaurio <j.chavez.perez.89@gmail.com>
2018-02-17 00:54:26 -0600

    Agregar el botón para descargar imagen

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -1,16 +1,27 @@
 package mx.unam.aragon.fes.diplo.gitandroid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button miBoton, miBoton2;
+    private Button miBoton, miBoton2, miBoton3;
     private TextView miTexto;
+    private ImageView miImagen;
+    private Bitmap imagendescargada;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         miBoton = findViewById(R.id.button);
         miBoton2 = findViewById(R.id.button2);
+        miBoton3 = findViewById(R.id.button3);
         miTexto = findViewById(R.id.textView);
+        miImagen = findViewById(R.id.imagen);
+
 
         miBoton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +54,41 @@ public class MainActivity extends AppCompatActivity {
                 miTexto.setText("Diplomado Android");
             }
         });
+
+        miBoton3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    new ObtenerImagen().execute(new URL ("https://vignette.wikia.nocookie.net/toontown/images/8/8a/Toontown_Online_Game_Screenshot_Toons.png"));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+
+
+    class ObtenerImagen extends AsyncTask<URL,Integer,Bitmap> {
+        @Override
+        protected Bitmap doInBackground(URL... urls) {
+            try{
+                imagendescargada = BitmapFactory.decodeStream(urls[0].openConnection().getInputStream());
+            }catch (Exception e){
+                Log.e("diplo", "Error"+e.toString());
+            }
+            return imagendescargada;
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            //establecer eÃ± ImageView sobre el layout
+            miImagen.setImageBitmap(imagendescargada);
+            super.onPostExecute(bitmap);
+        }
+
+    }
+
+
 
 }
